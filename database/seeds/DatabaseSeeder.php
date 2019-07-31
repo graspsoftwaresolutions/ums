a348a0fb8b05d93fdc48d638a9fc6e0a6a92cfb5
shyni-bizsoft <shyni.bizsoft@gmail.com>
2019-07-31 18:26:39 +0800

    subscrition

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,23 +14,23 @@ class DatabaseSeeder extends Seeder
         // factory(App\User::class, 5000)->create()->each(function ($user) {
 
         // });
-		$this->call(PermissionTableSeeder::class);
-		$this->call(RoleTableSeeder::class);
-		$this->call(CountryTableSeeder::class);
-        $this->call(StateTableSeeder::class);
-        $this->call(CityTableSeeder::class);
-		$this->call(UserTableSeeder::class);
-        $this->call(CompanyTableSeeder::class);
-        $this->call(UnionBranchTableSeeder::class);
-        $this->call(CompanyBranchTableSeeder::class);
+		// $this->call(PermissionTableSeeder::class);
+		// $this->call(RoleTableSeeder::class);
+		// $this->call(CountryTableSeeder::class);
+        // $this->call(StateTableSeeder::class);
+        // $this->call(CityTableSeeder::class);
+		// $this->call(UserTableSeeder::class);
+        // $this->call(CompanyTableSeeder::class);
+        // $this->call(UnionBranchTableSeeder::class);
+        // $this->call(CompanyBranchTableSeeder::class);
 		
 		// new master seeds
-        $this->call(RelationshipTableSeeder::class);
-        $this->call(FeeTableSeeder::class);
-        $this->call(DesignationTableSeeder::class);
-        $this->call(RaceTableSeeder::class);
-        $this->call(PersonTitleTableSeeder::class);
-        $this->call(StatusTableSeeder::class);
-        $this->call(FormTypeTableSeeder::class);
+        // $this->call(RelationshipTableSeeder::class);
+        // $this->call(FeeTableSeeder::class);
+        // $this->call(DesignationTableSeeder::class);
+        // $this->call(RaceTableSeeder::class);
+        // $this->call(PersonTitleTableSeeder::class);
+        // $this->call(StatusTableSeeder::class);
+        // $this->call(FormTypeTableSeeder::class);
     }
 }
