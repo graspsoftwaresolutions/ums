5dda20aee239ebffe796517de49636a6e75df4e2
Mounika <mounika.Bizsoft@gmail.com>
2019-08-07 10:54:32 +0530

    sub payment design

--- resources/views/subscription/subscription_payment.blade.php
+++ resources/views/subscription/subscription_payment.blade.php
@@ -15,10 +15,7 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('public/assets/css/pages/data-tables.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('public/assets/css/pages/form-wizard.css') }}">
 <style>
-.filter{
-    padding-top: 9px;
-    background-color: #dad1d1c7;
-}
+
 </style>
 @endsection
 @section('main-content')
@@ -39,7 +36,7 @@
                             <div class="container">
                                 <div class="row">
                                     <div class="col s10 m6 l6">
-                                        <h5 class="breadcrumbs-title mt-0 mb-0">{{__('Member Subscription List')}}</h5>
+                                        <h5 class="breadcrumbs-title mt-0 mb-0">{{__('Member Subscription Payment')}}</h5>
                                         <ol class="breadcrumbs mb-0">
                                             <li class="breadcrumb-item"><a
                                                     href="{{ route('home', app()->getLocale())  }}">{{__('Dashboard')}}</a>
@@ -56,29 +53,52 @@
                         <div class="row">
                         <div class="col s12">
                         <div class="card">
-                            <div class="card filter">
                             
-                            <form method="post" id="filtersubmit">
-                            @csrf
-                                <div class="row">                          
-                                    <div class="input-field col s4">
-                                        <i class="material-icons prefix">date_range</i>
-                                        <input id="from_date" type="text" required class="validate datepicker" name="from_date">
-                                        <label for="from_date">{{__('From Month and Year')}}</label>
-                                    </div>
-                                    <div class="input-field col s4">
-                                        <i class="material-icons prefix">date_range</i>
-                                        <input id="to_date" type="text" required class="validate datepicker" name="to_date">
-                                        <label for="to_date">{{__('To Month and Year')}}</label>
-                                    </div>
-                                    <div class="input-field col s4">
-                                    <input type="submit"  class="btn" name="search" value="{{__('Search')}}">
-                                    </div>
-                                </div>
-                            </form>  
+                            
+								<form method="post" id="filtersubmit">
+									@csrf
+									<div class="row">                          
+										<div class="input-field col s4">
+											<i class="material-icons prefix">date_range</i>
+											<input id="from_date" type="text" class="validate datepicker" name="from_date">
+											<label for="from_date">{{__('From Month and Year')}}</label>
+										</div>
+										<div class="input-field col s4">
+											<i class="material-icons prefix">monetization_on</i>
+												<input id="amount" type="text" name="amount" class="">
+											<label for="amount">{{__('Amount')}}</label>
+										</div>
+										<div class="input-field col s4">
+										<input type="submit"  class="btn" name="pay" value="{{__('Pay')}}">
+										</div>
+									</div>
+								</form>  
                             </div>
-                           
-                          </div>
+							<div class="row hide">
+                                <div class="col s12">
+                                    <!-- Horizontal Stepper -->
+									<div class="card">
+                                    <div class="col sm12 m12">
+                                        <table id="page-length-option" class="display ">
+                                            <thead>
+                                            <tr>
+                                            <th>{{__('Month and Year')}}</th>
+                                            <th>{{__('Amount')}}</th>
+                                            <th>{{__('Status')}}</th>
+                                            </tr>
+											<tr>
+												<td>xx</td>
+												<td>xx</td>
+												<td>xx</td>
+                                            </tr>
+                                            </thead>
+                                        </table>
+                                        </div>          
+									</div>
+									</div>
+								</div>
+                            </div>
+                        
                         </div>
                     </div>
                        
@@ -109,8 +129,8 @@
 <script src="{{ asset('public/assets/js/scripts/form-wizard.js') }}" type="text/javascript"></script>
 <script>
 $("#subscriptions_sidebars_id").addClass('active');
-$("#subcomp_sidebar_li_id").addClass('active');
-$("#subcomp_sidebar_a_id").addClass('active');
+$("#subscriptionpayment_sidebar_li_id").addClass('active');
+$("#subscriptionpayment_sidebar_a_id").addClass('active');
 
 	$(document).ready(function(){
 		//loader.showLoader();
@@ -144,37 +164,35 @@ $("#subcomp_sidebar_a_id").addClass('active');
 	  
 	   return true;
 	}
-    $("#filtersubmit").validate({
+    $('#filtersubmit').validate({
     rules: {
         from_date: {
         required: true,
-        
-      },
-      to_date: {
+    },
+    amount: {
         required: true,
-        
-      },
-      
-      //For custom messages
-      messages: {
-        from_date:{
-        required: "Enter From Date"
-      },
-      to_date:{
-        required: "Enter To Date"
-      },
-      
-      errorElement : 'div',
-      errorPlacement: function(error, element) {
-        var placement = $(element).data('error');
-        if (placement) {
-          $(placement).append(error)
-        } else {
-      error.insertAfter(element);
-      }
+        digits: true,
+    },
+    },
+    //For custom messages
+    messages: {
+        from_date: {
+        required: '{{__("Please select from date") }}',
+    },
+    amount: {
+        required: '{{__("Please Enter Amount") }}',
+        digits: '{{__("Numbers only") }}',
+    },
+    },
+    errorElement: 'div',
+    errorPlacement: function(error, element) {
+    var placement = $(element).data('error');
+    if (placement) {
+        $(placement).append(error)
+    } else {
+        error.insertAfter(element);
     }
-  });
-/*
-
+    }
+});
 </script>
 @endsection
