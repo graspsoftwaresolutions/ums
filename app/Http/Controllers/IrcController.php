0d3e46338e17ed849be710658cebfb5093a24836
Mounika <mounika.Bizsoft@gmail.com>
2019-08-16 09:23:46 +0530

    irc save

--- app/Http/Controllers/IrcController.php
+++ app/Http/Controllers/IrcController.php
@@ -352,6 +352,9 @@ class IrcController extends CommonController
 		
 		if(!empty($request->id))
 		{
+			// echo "<pre>";
+			//  print_r($request->all());
+			//  exit;
 			if($user_role=='irc-confirmation')
 			{		
 				$saveIrc = $this->Irc->saveIrcdata($data);
