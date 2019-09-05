c83c3959dd9d84202f8224f6751780ff3417d2ce
Murugan Bzsoft <murugan.bizsoft@gmail.com>
2019-09-05 17:28:47 +0530

    comment member number

--- resources/views/membership/add_membership.blade.php
+++ resources/views/membership/add_membership.blade.php
@@ -184,7 +184,7 @@
 																	</div> 
 																	<div class="input-field col s12 m6" style="display:none;" id="member_old_div">
 																		<input type="text" name="old_mumber_number" value="{{ old('old_mumber_number') }}" id="old_mumber_number" class="autocomplete">
-																		<input type="text" name="old_member_id" value="" id="old_member_id" class="autocomplete">
+																		<input type="text" name="old_member_id" value="" id="old_member_id" class="autocomplete hide">
 																		<label for="old_mumber_number">{{__('Old Number') }}</label>
 																		<span> 
 																		</span>
