d925e5162571290a4a846d627def26e901c1e3de
Murugan <murugan.bizsoft@gmail.com>
2019-12-06 12:40:28 +0800

    statement changes

--- resources/views/reports/common_member_statement.blade.php
+++ resources/views/reports/common_member_statement.blade.php
@@ -16,7 +16,7 @@
 					<td width="79%" style="padding: 2px;">
 						<span class="report-address" style="font-weight: bold;font-size:18px;">NATIONAL UNION OF BANK EMPLOYEES,PENINSULAR MALAYSIA</span>
 				
-						<p style="padding-top: 0px;margin-top: 5px;">MEMBERS ANNUAL STATEMENT OF ACCOUNT OF BENEVOLENT FUND</p>
+						<p style="padding-top: 0px;margin-top: 5px;">MEMBER'S ANNUAL STATEMENT OF ACCOUNT OF BENEVOLENT FUND</p>
 					</td>
 					<td width="20%" style="padding: 2px;">
 						<br>
@@ -99,16 +99,16 @@
 		
 			<tr >
 				<!--td style="border: 1px solid #988989 !important; ">{{ $sno }}</td-->
-				<td style="border: 1px solid #988989 !important;">{{ $member->member_number }}</td>
+				<td style="border: 1px solid #988989 !important;text-align: center;">{{ $member->member_number }}</td>
 				<td style="border: 1px solid #988989 !important;">{{ $member->name }}</td>
-				<td class="nric_no" style="border: 1px solid #988989 !important;">{{ $ic }}</td>
-				<td style="border: 1px solid #988989 !important;">{{ !empty($last_history) ? $last_history->ACCSUBSCRIPTION : 0 }}</td>
-				<td style="border: 1px solid #988989 !important;" >{{ !empty($history) ? $history->TOTALSUBCRP_AMOUNT : 0 }}</td>
-				<td  style="border: 1px solid #988989 !important;">{{ $lastallsubs+$currentsubs }}</td>
-				<td style="border: 1px solid #988989 !important;">{{  $acc_bf }}</td>
-				<td style="border: 1px solid #988989 !important;">{{  $data['insurance_count'] }}</td>
-				<td style="border: 1px solid #988989 !important;">{{  $total_dues }}</td>
-				<td style="border: 1px solid #988989 !important;">{{  $subsdues }}</td>
+				<td class="nric_no" style="border: 1px solid #988989 !important;text-align: center;">{{ $ic }}</td>
+				<td style="border: 1px solid #988989 !important;text-align: center;">{{ !empty($last_history) ? $last_history->ACCSUBSCRIPTION : 0 }}</td>
+				<td style="border: 1px solid #988989 !important;text-align: center;" >{{ !empty($history) ? $history->TOTALSUBCRP_AMOUNT : 0 }}</td>
+				<td  style="border: 1px solid #988989 !important;text-align: center;">{{ $lastallsubs+$currentsubs }}</td>
+				<td style="border: 1px solid #988989 !important;text-align: center;">{{  $acc_bf }}</td>
+				<td style="border: 1px solid #988989 !important;text-align: center;">{{  $data['insurance_count'] }}</td>
+				<td style="border: 1px solid #988989 !important;text-align: center;">{{  $total_dues }}</td>
+				<td style="border: 1px solid #988989 !important;text-align: center;">{{  $subsdues }}</td>
 				
 			</tr> 
 				
@@ -120,12 +120,12 @@
 		<tr>
 			<td width="5%" style="vertical-align: top;">NOTE: </td>
 			<td>
-				<p style="padding-top: 0px;margin-top: 0px;">1) Any queries on this statement should be communicated to the Hon. Gen Secretary within 14 days upon receipt of this statement, otherwise it would be treated os correct </p>
+				<p style="padding-top: 0px;margin-top: 0px;">1) Any queries on this statement should be communicated to the Hon. Gen Secretary within 14 days upon receipt of this statement, otherwise it would be treated as correct. </p>
 				<p>2) If you wish to change the nominee, kindly request for the nominee form from you Branch Secretary.</p>
 				<!--p>3) The Accrued Benefit will be payable according to Rule 6 of the Benevolent Fund Rules.</p-->
 				<p>3) If arrears indicates less than 2 months then it may be due to transit of payment from Bank to NUBE Headquarters.</p>
 				<br>
-				<p align="center">National Union of Bonk Employees, 12 NUBS House, 3rd Floor Jalan Tun Sambanthan 3, BrIckfields 50470 Kuala Lumpur.</p>
+				<p align="center">National Union of Bonk Employees, 12 NUBE House, 3rd Floor Jalan Tun Sambanthan 3, BrIckfields 50470 Kuala Lumpur.</p>
 				<p align="center">
 					Tel: (603) 2274 9800 &nbsp;&nbsp; Fox: (603) 2260 1800 &nbsp;&nbsp; Email: nube_hq@nube.org.my &nbsp;&nbsp; Website: www.nube.org.my
 				</p>
