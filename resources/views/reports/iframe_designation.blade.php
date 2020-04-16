2ab59964fe572eb34a9c4ae84dd9f374044992b4
Murugan Bizsoft <murugan.bizsoft@gmail.com>
2020-04-16 12:56:19 +0530

    add pdf excel to designation

--- resources/views/reports/iframe_designation.blade.php
+++ resources/views/reports/iframe_designation.blade.php
@@ -165,8 +165,8 @@
 					
 				</td>
 				<td width="20%">	
-					<a href="#" class="export-button btn btn-sm exportToExcel hide" style="background:#227849;"><i class="material-icons">explicit</i></a>
-					<a href="{{ url(app()->getLocale().'/export-pdf-branch-status?offset=0'.$searchfilters) }}" class="export-button btn btn-sm hide" style="background:#ff0000;"><i class="material-icons">picture_as_pdf</i></a>
+					<a href="#" class="export-button btn btn-sm exportToExcel " style="background:#227849;"><i class="material-icons">explicit</i></a>
+					<a href="{{ url(app()->getLocale().'/export-pdf-designation?offset=0'.$searchfilters) }}" class="export-button btn btn-sm " style="background:#ff0000;"><i class="material-icons">picture_as_pdf</i></a>
 					<a href="#" class="export-button btn btn-sm" style="background:#ccc;" onClick="window.print()"><i class="material-icons">print</i></a>
 				</td>
 			</tr>
---
+++ resources/views/reports/pdf_designation.blade.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<link rel="stylesheet" type="text/css" href="{{ asset('public/assets/css/vendors.min.css') }}">
+	<!-- <link rel="stylesheet" type="text/css" href="{{ asset('public/assets/css/vertical-modern-menu.css') }}"> -->
+	<link rel="stylesheet" type="text/css" href="{{ asset('public/assets/css/materialize.css') }}">
+	<style>
+		/* Styles go here */
+		
+		tr {
+		    border-bottom: none !important; 
+		}
+
+		.page-header, .page-header-space {
+		  height: 100px;
+		  z-index:999;
+		}
+		
+		.page-footer, .page-footer-space {
+		  height: 50px;
+		
+		}
+		
+		.page-footer {
+		  position: fixed;
+		  bottom: 0;
+		  width: 100%;
+		  //border-top: 1px solid black; /* for demo */
+		  background: #fff; /* for demo */
+		  color:#000;
+		}
+		
+		.page-header {
+		  position: fixed;
+		  top: 0mm;
+		  width: 100%;
+		  background: #fff; /* for demo */
+		  color:#000;
+		}
+		
+		.page {
+		  page-break-after: always;
+		}
+		
+		@page {
+		  margin: 3mm
+		}
+		
+		@media print {
+			@page {
+				size: landscape; 
+				margin: 3mm;
+			}
+		    thead {display: table-header-group;} 
+		    tfoot {display: table-footer-group;}
+		   
+		    button {display: none;}
+		   
+		    body {margin: 0;}
+			.export-button{
+				display:none !important;
+			}
+			.page-header, .page-header-space {
+			  height: 70px;
+			  z-index:999;
+			}
+			.page-header,.page-table-header-space {
+			  //background: #fff; /* for demo */
+			  //color:#000;
+			}
+			#page-length-option {
+			  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+			  border-collapse: collapse;
+			  width: 100%;
+			}
+
+			#page-length-option td, #page-length-option th {
+			  //border: 1px solid #ddd !important;
+			  padding: 4px;
+			}
+			
+
+			.page-header-area{
+				display: none;
+			}
+			
+		}
+		@media not print {
+			table {
+			    display: table;
+			    width: 100%;
+			    border-spacing: 0;
+			    border-collapse: none;
+			}
+			.page-table-header-space {
+			  width: 100%;
+			 // position: fixed;
+			  top:101px;
+			  margin-bottom:20px;
+			  background: #343d9f; /* for demo */
+			  z-index:999;
+			  color:#fff;
+			  font-size: 14px;
+			}
+			.tbody-area{
+				top:140px;
+				//position: absolute;
+			}
+			.nric_no{
+				width:150px !important;
+			}
+			.page-header-area{
+				display: none;
+			}
+		}
+		td, th {
+			display: table-cell;
+			padding: 7px 5px;
+			text-align: left;
+			vertical-align: middle;
+			//border-radius: 2px;
+		}
+		.btn, .btn-large, .btn-small, .btn-flat {
+			line-height: 36px;
+			display: inline-block;
+			height: 35px;
+			padding: 0 7px;
+			vertical-align: middle;
+			text-transform: uppercase;
+			border: none;
+			border-radius: 4px;
+			-webkit-tap-highlight-color: transparent;
+		}
+		.tbody-area{
+			color:#000;
+		}
+		#page-length-option td, #page-length-option th {
+			//border: 1px solid #ddd !important;
+			padding: 4px;
+		}
+		html {
+
+		    //font-family: 'Muli', sans-serif;
+		    font-weight: normal;
+		    line-height: 1; 
+		    color: rgba(0, 0, 0, .87);
+		    font-size: 12px;
+		}
+		.nric_no{
+			width:10% !important;
+		}
+		
+		.report-address{
+			font-weight:bold;
+			font-size:14px;
+		}
+
+		
+		
+	</style>
+	<script type="text/javascript">
+	
+	</script>
+</head>
+
+<body>
+	
+	@include('reports.common_designation')
+	
+</body>
+
+
+</html>
