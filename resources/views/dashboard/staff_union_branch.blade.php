702f8c2c3ebf0e995e2340ac2f17a18d56110775
Murugan Bizsoft <murugan.bizsoft@gmail.com>
2020-12-09 10:17:39 +0530

    enable counts staff

--- resources/views/dashboard/staff_union_branch.blade.php
+++ resources/views/dashboard/staff_union_branch.blade.php
@@ -40,7 +40,7 @@
    <div class="row">
    		 <div class="col s12 m6 l1">
    		 </div>
-		<div class="col s12 m6 l2 hide">
+		<div class="col s12 m6 l2 ">
 			<a style="color:white" >
 			 <div class="card animate fadeRight">
 				<div class="card-content green lighten-1 white-text">
@@ -53,7 +53,7 @@
 			 </div>
 			</a>
 		</div>
-		<div class="col s12 m6 l2 hide">
+		<div class="col s12 m6 l2 ">
 			<a style="color:white">
 			 <div class="card animate fadeLeft">
 				<div class="card-content red accent-2 white-text">
@@ -68,21 +68,8 @@
 			</a>
 		</div>
 		<div class="dash-tab-clearfix"/>
-	      <div class="col s12 m6 l3">
-	      	<a style="color:white" href="{{ url(app()->getLocale().'/membership_list?type=1') }}"> 
-	         <div class="card animate fadeRight">
-	            <div class="card-content orange lighten-1 white-text">
-	               <p class="card-stats-title"><i class="material-icons"></i>{{__('No of Rejected Members') }}</p>
-	               <h4 class="card-stats-number white-text">{{ $rejected_count }}</h4>
-	            </div>
-	            <div class="card-action orange">
-	               <div id="invoice-line" class="center-align">{{__('Rejected Members List') }}</div>
-	            </div>
-	         </div>
-	        </a>
-	      </div>
 	     
-	      <div class="col s12 m6 l3">
+	      <div class="col s12 m6 l2">
 	      	<a style="color:white" href="{{ route('master.addmembership', app()->getLocale())  }}">
          <div class="card animate fadeRight">
             <div class="card-content pink lighten-1 white-text">
@@ -96,7 +83,7 @@
          </a>
 	        
 	      </div>
-	       <div class="col s12 m6 l3">
+	       <div class="col s12 m6 l2">
         <a style="color:white" href="{{ route('resignation.add', app()->getLocale())  }}">
 	         <div class="card animate fadeRight" style="">
 	            <div class="card-content blue lighten-1 white-text">
@@ -108,5 +95,18 @@
 	         </div>
 	         </a>
      	 </div>
+     	 <div class="col s12 m6 l2">
+	      	<a style="color:white" href="{{ url(app()->getLocale().'/membership_list?type=1') }}"> 
+	         <div class="card animate fadeRight">
+	            <div class="card-content orange lighten-1 white-text">
+	               <p class="card-stats-title"><i class="material-icons"></i>{{__('No of Rejected Members') }}</p>
+	               <h4 class="card-stats-number white-text">{{ $rejected_count }}</h4>
+	            </div>
+	            <div class="card-action orange">
+	               <div id="invoice-line" class="center-align">{{__('Rejected Members List') }}</div>
+	            </div>
+	         </div>
+	        </a>
+	      </div>
    </div>
 </div>
