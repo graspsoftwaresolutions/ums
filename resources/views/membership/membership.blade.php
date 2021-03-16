4143277e9fbf8704e03d2f6b63cf8c1d102d4932
muruganbizsoft <murugan.bizsoft@gmail.com>
2021-03-16 17:02:03 +0530

    fix union members export

--- resources/views/membership/membership.blade.php
+++ resources/views/membership/membership.blade.php
@@ -616,7 +616,7 @@ $(function () {
 			   extend: 'pdf',
 			   footer: true,
 			   exportOptions: {
-					columns: []
+					columns: [0, 1,2,3,4,5]
 				},
                 title : 'Members List', 
                 text: '<i class="fa fa-file-pdf-o"></i>',
@@ -626,7 +626,7 @@ $(function () {
 			   extend: 'excel',
 			   footer: false,
 			   exportOptions: {
-					columns: []
+					columns: [0, 1,2,3,4,5]
 				},
 				title : 'Members List',
                 text:    '<i class="fa fa-file-excel-o"></i>',
@@ -636,11 +636,46 @@ $(function () {
 			   extend: 'print',
 			   footer: false,
 			   exportOptions: {
-					columns: []
+					columns: [0, 1,2,3,4,5]
 				},
 				title : 'Members List',
                 text:   '<i class="fa fa-files-o"></i>',
-                titleAttr: 'print'
+                titleAttr: 'print',
+                pageSize: 'LEGAL',
+                 customize: function(win)
+		            {
+		 
+		                var last = null;
+		                var current = null;
+		                var bod = [];
+		 
+		                var css = '@page {  font-size:10px; padding:10px; margin: 5px;}',
+		                    head = win.document.head || win.document.getElementsByTagName('head')[0],
+		                    style = win.document.createElement('style');
+		 
+		                style.type = 'text/css';
+		                style.media = 'print';
+		 
+		                if (style.styleSheet)
+		                {
+		                  style.styleSheet.cssText = css;
+		                } 
+		                else
+		                {
+		                  style.appendChild(win.document.createTextNode(css));
+		                }
+		 
+		                head.appendChild(style);
+
+		                $(win.document.body).css( 'font-size', '9pt' );
+		                $(win.document.body).css( 'margin', '10pt' );
+		                $(win.document.body).find( 'table' )
+                        .addClass( 'compact' )
+                        .css( 'font-size', 'inherit' );
+                        $(win.document.body).find('h1').css('font-size', '16pt');
+                        $(win.document.body).find('h1').css('padding', '3pt');
+                        $(win.document.body).find('h1').css('margin', '4pt');
+		         }
 		   }  
 		],
 	"processing": true,
